6c87031f17030d13b9b3cb5efd2ce265e7e40769
judgewooden <douwe.jong@gmail.com>
2022-06-26 17:28:12 +0200

    Final Version

--- main.py
+++ main.py
@@ -10,35 +10,56 @@ r = redis.Redis(decode_responses=True)
 
 @app.route('/')
 def root():
-    return render_template("index.html")
+    mylist=[]
+    for k in r.keys("watch:*"):
+        mylist.append(k[6:])
+    return render_template('index.html', watches=mylist)
+
+# @app.route('/watch/<watch_id>')
+#     return render_template("index.html")
 
 @app.route('/get')
 def api_get():
-    id = request.args.get('i')
+    id = request.args.get('id')
 
     answer={}
     answer['id'] = id
     answer['lastupdated'] = r.hget("watch:" + id, "lastupdated")
+    if answer['lastupdated'] is None:
+        if app.config["DEBUG"]:
+            print(id, "NOT FOUND")
+        return "", 404
+
     answer['longitude'] = r.hget("watch:" + id, "longitude")
     answer['latitude'] = r.hget("watch:" + id, "latitude")
 
-    print(answer)
+    if app.config["DEBUG"]:
+        print("get", answer)
     return json.dumps(answer), 200
 
 @app.route('/set')
 def api_set():
-    id = request.args.get('i')
-    latitude = float(request.args.get('a'))
-    longitude = float(request.args.get('b'))
-    print(id, longitude, latitude )
+    if app.config["DEBUG"]:
+        print("set", request.args )
     
+    try:
+        id = request.args.get('id')
+        latitude = float(request.args.get('latitude'))
+        longitude = float(request.args.get('longitude'))
+        if id is None or latitude is None or longitude is None:
+            raise Exception("BAD REQUEST")
+    except Exception as e:
+        if app.config["DEBUG"]:
+            print("ERROR", e)
+        return 'ERROR' + e, 400
+
+
     now = datetime.datetime.now()
-    r.zrem("watch", id)
+
     r.hset("watch:" + id, "lastupdated", now.strftime("%Y%m%d%H%M%S") )
     r.hset("watch:" + id, "longitude", longitude)
     r.hset("watch:" + id, "latitude", latitude)
-
-    return 'OK', 200
+    return '', 200
 
 app.run()
 
